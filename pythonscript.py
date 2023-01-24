6d3bc8b367bc29f7ec9e26798e289b40b01e8102
Adnan <adnan@Adnans-Air.localdomain>
2023-01-24 09:28:40 +0100

    Init commit

---
+++ pythonscript.py
@@ -0,0 +1,21 @@
+print("Kako ste Envere?")
+
+enversMood="good"
+if enversMood == "good":
+    print("Dobar dan")
+else:
+    print("Nisam dobro")
+
+for i  in range(10):
+        print("Kako ste?")
+
+
+import random
+
+list = ["nova ljetna vantura", "radim samo sta me fura"]
+random.shuffle(list)
+
+for i in range(10):
+    for i in range:
+        print("*")
+        print
